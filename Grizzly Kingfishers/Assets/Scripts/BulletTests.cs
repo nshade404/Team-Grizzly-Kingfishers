0bc4931090f07fa8376d3e7252b94521965c595d
Andrew Phipps <92318426+nshade404@users.noreply.github.com>
2024-03-09 14:50:51 -0600

    Playing in my sandbox for an idea of bullet sets

---
+++ Grizzly Kingfishers/Assets/Scripts/BulletTests.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTests : MonoBehaviour
+{
+    enum Bullets
+    {
+        Poison,
+        Fire,
+        Ice,
+        Stone,
+        Electric,
+        PocketSand
+    }
+
+    [SerializeField] Bullets bulletChoice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
