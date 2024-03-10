d58246f995e0a1a4cd25a335c7e32a50e85c0939
Andrew Phipps <92318426+nshade404@users.noreply.github.com>
2024-03-10 12:40:33 -0500

    updated implementation for bullet type and number.
    swaps to only ammo in inventory and swaps only one type per click

--- Grizzly Kingfishers/Assets/Scripts/BulletTests.cs
+++ Grizzly Kingfishers/Assets/Scripts/BulletTests.cs
@@ -6,6 +6,7 @@ public class BulletTests : MonoBehaviour
 {
     enum Bullets
     {
+        None,
         Poison,
         Fire,
         Ice,
@@ -26,6 +27,7 @@ public class BulletTests : MonoBehaviour
     void Start()
     {
         //inventory check for bullet types <----
+        
     }
 
     // Update is called once per frame
@@ -42,23 +44,223 @@ public class BulletTests : MonoBehaviour
         isSwapping = true;
             switch (bulletChoice)
             {
-                case Bullets.Poison:
-                    bulletChoice = Bullets.Fire;
-                    break;
-                case Bullets.Fire:
-                    bulletChoice = Bullets.Ice;
-                    break;
-                case Bullets.Ice:
-                    bulletChoice = Bullets.Stone;
-                    break;
-                case Bullets.Stone:
-                    bulletChoice = Bullets.Electric;
-                    break;
-                case Bullets.Electric:
-                    bulletChoice = Bullets.PocketSand;
-                    break;
-                case Bullets.PocketSand:
+                case Bullets.None:
+                if (numPoison > 0)
+                {
                     bulletChoice = Bullets.Poison;
+                }
+                else if (numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else if (numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+            case Bullets.Poison:
+                    if(numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else if(numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else if (numPoison > 0)
+                {
+                    bulletChoice = Bullets.Poison;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+                case Bullets.Fire:
+                
+                if (numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else if (numPoison > 0)
+                {
+                    bulletChoice = Bullets.Poison;
+                }
+                else if (numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+                case Bullets.Ice:
+                
+                if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else if (numPoison > 0)
+                {
+                    bulletChoice = Bullets.Poison;
+                }
+                else if (numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else if (numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+                case Bullets.Stone:
+                
+                if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else if (numPoison > 0)
+                {
+                    bulletChoice = Bullets.Poison;
+                }
+                else if (numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else if (numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+                case Bullets.Electric:
+                
+                if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else if (numPoison > 0)
+                {
+                    bulletChoice = Bullets.Poison;
+                }
+                else if (numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else if (numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+                case Bullets.PocketSand:
+
+                if(numPoison > 0)
+                {
+                    bulletChoice = Bullets.Poison;
+                }
+                else if (numFire > 0)
+                {
+                    bulletChoice = Bullets.Fire;
+                }
+                else if (numIce > 0)
+                {
+                    bulletChoice = Bullets.Ice;
+                }
+                else if (numStone > 0)
+                {
+                    bulletChoice = Bullets.Stone;
+                }
+                else if (numElectric > 0)
+                {
+                    bulletChoice = Bullets.Electric;
+                }
+                else if (numSand > 0)
+                {
+                    bulletChoice = Bullets.PocketSand;
+                }
+                else
+                {
+                    bulletChoice = Bullets.None;
+                }
+                break;
+                default:
+                    bulletChoice = Bullets.None;
                     break;
 
             }
