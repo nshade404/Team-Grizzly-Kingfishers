31c32cdf57b0be378114a440d108751c727d694e
Jiopio <jcalhoun88@gmail.com>
2024-03-11 17:13:09 -0400

    EnemyAI and Player updates
    EnemyAI - tapped into gameManager to use those functionalities and replace the local capturing of player.
    Updated aggro check from a distance to player to a SphereCollider for a more efficient system.
    Minor tweaks on PlayerController

--- Grizzly Kingfishers/Assets/Scripts/playerController.cs
+++ Grizzly Kingfishers/Assets/Scripts/playerController.cs
@@ -31,8 +31,7 @@ public class playerController : MonoBehaviour, IDamage {
 
     // Update is called once per frame
     void Update() {
-        //if (!GameManager.instance.isPaused) {
-        if (true) {
+        if (!gameManager.instance.isPaused) {
 #if UNITY_EDITOR 
             Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.blue);
 #endif
