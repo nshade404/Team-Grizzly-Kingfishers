db53036595a5ba25bfed7568888b1c463d72b228
camodude120 <waszafire5@gmail.com>
2024-03-12 23:18:13 -0700

    testing

---
+++ Grizzly Kingfishers/Assets/Scripts/Interactor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionpointRadius = 0.5f;
+    [SerializeField] private LayerMask _interactableMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private int _numFound;
+
+    private void Update()
+    {
+        _numFound = Physics.OverlapBoxNonAlloc(_interactionPoint.position,
+                                               _interactionpointRadius,
+                                               _colliders,
+                                               _interactableMask);
+
+    }
+}
