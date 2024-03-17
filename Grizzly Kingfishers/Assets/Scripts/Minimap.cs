8752f02d26bc9b395ef599f2ce24cd22c83d041b
camodude120 <waszafire5@gmail.com>
2024-03-17 14:17:04 -0700

    got the minimap to work

--- Grizzly Kingfishers/Assets/Scripts/Minimap.cs
+++ Grizzly Kingfishers/Assets/Scripts/Minimap.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
-    public Transform player;
+    private gameManager gameManagerInstance; // Reference to the GameManager
 
+    private void Awake()
+    {
+        gameManagerInstance = gameManager.instance; // Get the instance of the GameManager
+    }
 
     private void LateUpdate()
     {
-        Vector3 newPos = player.position;
-        newPos.y = transform.position.y;
-        transform.position = newPos;
+        if (gameManagerInstance != null && gameManagerInstance.player != null)
+        {
+            Transform player = gameManagerInstance.player.transform;
 
-        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+            // Update minimap position
+            Vector3 newPos = player.position;
+            newPos.y = transform.position.y;
+            transform.position = newPos;
+
+            // Update minimap rotation
+            transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        }
+        
     }
-
-
-
-
-
-
-
 }
-    
+
+
