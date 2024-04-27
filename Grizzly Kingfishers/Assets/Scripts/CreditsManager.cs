f7551f51590173e99db27f806b445ebcc0f7dd1e
Damien Remington <DJRemington@student.fullsail.edu>
2024-04-27 17:58:05 -0400

    text file and basic scrolling for credits menu implemented

---
+++ Grizzly Kingfishers/Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class CreditsManager : MonoBehaviour
+{
+    public Text creditsText;
+    public string creditsFilePath = "Assets/RocketAwayCredits.txt";
+    public float scrollSpeed = 1f;
+    public float scrollSensitivity = 1f;
+
+    private string[] creditLines;
+    private float startYPos;
+    private float currentYPos;
+
+    void Start()
+    {
+        LoadCredits();
+    }
+
+    void LoadCredits()
+    {
+        if (File.Exists(creditsFilePath))
+        {
+            creditLines = File.ReadAllLines(creditsFilePath);
+            string creditsContent = string.Join("\n", creditLines);
+            creditsText.text = creditsContent;
+
+            RectTransform rectTransform = creditsText.rectTransform;
+            startYPos = rectTransform.rect.height / 2f;
+            currentYPos = startYPos;
+        }
+        else
+        {
+            Debug.LogError("Credits file not found at path: " + creditsFilePath);
+        }
+    }
+}
